c00fcae62ce413baf65f1f1d25087a33fc2e88a6
professor <professor@L2MP.IPIRANGA.FATEC>
2018-04-19 13:30:43 -0300

    primeira versao

---
+++ ControleDeEmprestimo/teste/br/emprestimo/testeUnitario/UC06CadastrarLivro.java
@@ -0,0 +1,28 @@
+package br.emprestimo.testeUnitario;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.emprestimo.modelo.Livro;
+
+public class UC06CadastrarLivro {
+	public static Livro livro;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		livro = new Livro();
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	@Test(expected=RuntimeException.class)
+	public void CT01UC06CadastrarLivro_com_isbn_invalido_branco() {
+		livro.setIsbn("");
+	}
+	@Test(expected=RuntimeException.class)
+	public void CT02UC06CadastrarLivro_com_isbn_invalido_nulo() {
+		livro.setIsbn(null);
+	}
+}
